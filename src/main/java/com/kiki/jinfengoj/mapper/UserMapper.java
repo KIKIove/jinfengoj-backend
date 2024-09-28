b5f553ae4c27d1093435f1e42504f491fd72956e
noobFUNER <len_ye@qq.com>
2024-09-28 23:56:30 +0800

    项目初始化

---
+++ src/main/java/com/kiki/jinfengoj/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.kiki.jinfengoj.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.kiki.jinfengoj.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+   
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
